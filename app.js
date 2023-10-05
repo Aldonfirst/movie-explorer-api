acf9c2d36392c17ce4f8a8f51cb2b4a62bbbf67b
Aldonfirst <alchevck44@bk.ru>
2023-10-05 13:56:12 +0300

    port

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(cors({
   credentials: true,
   maxAge: 15,
-  origin: ['https://aldonmovie.nomoredomainsicu.ru', 'http://aldonmovie.nomoredomainsicu.ru', 'http://localhost:3001'],
+  origin: ['https://aldonmovie.nomoredomainsicu.ru', 'http://localhost:3001'],
 }));
 
 app.use(bodyParser.json());
