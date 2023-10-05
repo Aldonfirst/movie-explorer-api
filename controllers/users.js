8654ad02db130a2143312ea16425a8794f5f51fb
Aldonfirst <alchevck44@bk.ru>
2023-10-05 23:43:51 +0300

    возврат к начал

--- controllers/users.js
+++ controllers/users.js
@@ -18,7 +18,7 @@ module.exports.createUser = async (req, res, next) => {
     }
     const hash = await bcrypt.hash(password, 10);
     const user = await User.create({ name, email, password: hash });
-    const selectedUser = await User.findById(user._id);
+    const selectedUser = await User.findById(user._id).select('-password');
     res.status(201).send(selectedUser);
   } catch (err) {
     if (err.name === 'MongoServerError' && err.code === 11000) {
